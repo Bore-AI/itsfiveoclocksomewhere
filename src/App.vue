371d1a1ff56d1e8088ed9db2cbccbb7349baaa21
Fredrik Bore <fredrik@bore.ai>
2023-05-22 20:25:48 +0200

    feat: show country

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script lang="ts">
 import "leaflet/dist/leaflet.css";
+import "flag-icons/css/flag-icons.min.css";
 
 import cities from "@/assets/timezones.json";
 
 import moment from "moment-timezone";
+import * as isoCountries from "i18n-iso-countries";
+import * as enLang from "i18n-iso-countries/langs/en.json";
 
 import BackgroundMap from "@/components/BackgroundMap.vue";
+
+isoCountries.registerLocale(enLang);
 
 type City = {
   city: string;
@@ -24,26 +29,20 @@ const shuffleArray = <T>(array: T[]) => {
 
 let timezones: City[] = [];
 
-let debug = [];
-
 for (let index = 0; index < cities.length; index++) {
   const city = cities[index];
 
   moment().tz(city.timezone).hour() === 17 && timezones.push(city);
-
-  debug.push({
-    city: city.city,
-    hour: moment().tz(city.timezone).hour(),
-    timezone: city.timezone,
-  });
 }
-
-console.log(debug.sort((a, b) => a.hour - b.hour));
 
 shuffleArray(timezones);
 
 if (timezones.length > 0) {
   var fiveoclocktimezone = timezones[0];
+  var country = isoCountries.getName(fiveoclocktimezone.country, "en", {
+    select: "official",
+  });
+  console.log(country);
 } else {
   var fiveoclocktimezone = {
     city: "Nowhere",
@@ -60,6 +59,7 @@ export default {
   },
   data() {
     return {
+      country,
       fiveoclocktimezone,
     };
   },
@@ -69,11 +69,23 @@ export default {
 <template>
   <BackgroundMap :fiveoclocktimezone="fiveoclocktimezone" :zoom="5" />
   <div class="textContainer">
-    <div style="text-align: center; text-wrap: balance; user-select: none">
+    <div
+      style="
+        text-align: center;
+        text-wrap: balance;
+        user-select: none;
+        color: #94a3b8;
+      "
+    >
       It's five o'clock in
     </div>
-    <div style="color: white; font-size: xx-large">
+    <div style="color: #f1f5f9; font-size: xx-large">
       {{ fiveoclocktimezone.city }}
+    </div>
+
+    <div v-if="country" style="color: #e2e8f0; font-size: x-large">
+      <i :class="`fi fi-${fiveoclocktimezone.country.toLowerCase()}`"></i>
+      {{ country }}
     </div>
   </div>
 </template>
@@ -91,5 +103,6 @@ export default {
   justify-items: center;
   justify-content: center;
   font-size: x-large;
+  font-family: "Roboto Mono", monospace;
 }
 </style>
